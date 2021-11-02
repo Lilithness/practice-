068cdb993edcd13ac499de46ff6dc4e4e8ac0959
Layal Shaheen <88163555+Lilithness@users.noreply.github.com>
2021-11-01 23:59:47 -0700

    Update my_dna_project.py
    using context manager and command line arguments to deal with the files, and adding description

--- my_dna_project.py
+++ my_dna_project.py
@@ -1,64 +1,53 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+import sys
+import functions
 
-from functions import *
+def main(args:list) -> none:
+    """
+    assign the name of the file
+    :param: list that contains the name f the file
+    :return: none
+    """
+    filename = args[0]
 
-sequence = input("add the file name of the sequence") + ".txt"
-sequence = sequence.strip()
-
+with open ("filename", "r") as file:
+    """
+    open the file that has the DNA sequence and process it
+    """
+    contents = file.read()
+    contents = contents.strip()
+    dna = ''.join(contents)
+    dna = dna.upper()
+    dna = dna.replace("\n", "")
+    dna = dna.replace("\r", "")
+    dna = dna.replace(" ", "")
+    
 try:
-    f = open(sequence, "r")
+    """
+    try to open the file, send an error massage if it wasn't found
+    """
+    filename = open(dna, "r")
 except FileNotFoundError:
     print("sorry, file not found")
-    quit()
-dna = f.read()
-dna = dna.upper()
-dna = dna.replace("\n", "")
-dna = dna.replace("\r", "")
-dna = dna.replace(" ", "")
-def main():
-    print("What do you want to test the DNA sequence for?")
-    print("For Sickle cell anemia enter 1"
-          "\nTo exit enter 0")
-    return
-main()
 
 all_protiens_expected = []
-for prot in all_proteins_from_orfs(dna, 0, 0, True):
+"""
+make a list of protein sequences
+"""
+for prot in all_proteins_from_orfs(dna:str, 0, 0, True) -> list[list[str]]:
     all_protiens_expected.append(prot)
-#print(all_protiens_expected)
-type = int(input())
 
-class Data:
-    def __init__(self,disease,mutation_type):
-        self.disease = disease
-        self.mutation_types = mutation_type
-    def full_data(self):
-        return '{} {}'.format(self.disease, self.mutation_types)
-def check_data(type):
-    Q= input("do you want to check your data? Y or N")
-    if (Q=="y") or (Q=="Y"):
-        if type == 1:
-            print(data_1.full_data())
+for i in all_protiens_expected:
+    """
+    check if this string given, is in the list
+    """
+    if i == "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLVSRLQDRFKETNRNWACGDREDSWVSDRH":
+        print("the biological protein's index in the list is number ", all_protiens_expected.index(i),
+                "\nno sickle cell anemia")
 
-while (type!=0):
 
-    if type == 1:
-        for i in all_protiens_expected:
-            if i == "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLVSRLQDRFKETNRNWACGDREDSWVSDRH":
-                print("the biological protein's index in the list is number ", all_protiens_expected.index(i),
-                      "\nno sickle cell anemia")
-                data_1 = Data('normal,',' no_mutation')
-                check_data(1)
+    if i == "MVHLTPVEKSAVTALWGKVNVDEVGGEALGRLVSRLQDRFKETNRNWACGDREDSWVSDRH":
+        print(all_protiens_expected.index(i))
+        print("sickle cell anemia positive")
 
-            if i == "MVHLTPVEKSAVTALWGKVNVDEVGGEALGRLVSRLQDRFKETNRNWACGDREDSWVSDRH":
-                print(all_protiens_expected.index(i))
-                print("sickle cell anemia positive")
-                more_info = input("would you like to know more info about this genetic mutation? (Y) or (N)")
-                if (more_info == "Y" or more_info == "y"):
-                    print(sickle_cell_info)
-                data_1 = Data('sickle cell anemia positive,', 'point mutation')
-                check_data(1)
-    type = 0
-
-pass
+if __name__ == "__main__":
+    main(sys.argv[1:])
