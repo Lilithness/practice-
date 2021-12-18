105e8bb756507c6704f1ff0e90f46c9f260a48df
Layal Shaheen <88163555+Lilithness@users.noreply.github.com>
2021-12-19 00:18:55 +0300

    Set pandas options

--- sc_test.py
+++ sc_test.py
@@ -13,6 +13,10 @@ import os.path
 get_cmd_output("bio align /home/lilith/input/sample.fsa /home/lilith/input/query.fsa --vcf | column >alignment_result.vcf")
 #Run a shell command via subprocess and return exit code and stdout/stderr.
 
+pd.set_option('display.max_columns', 60)
+pd.set_option('display.max_rows',None)
+#Set pandas options
+
 url = "https://rest.ensembl.org/lookup/symbol/homo_sapiens/HBB"
 try:
     r = requests.get(url, headers={"Content-Type": "application/json"})
