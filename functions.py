ddc7eca0648363595bf621fef2a982aadf454a88
Layal Shaheen <88163555+Lilithness@users.noreply.github.com>
2021-12-18 23:57:03 +0300

    Add read_VCF() function

--- functions.py
+++ functions.py
@@ -2,6 +2,20 @@ from typing import List
 import info
 import requests
 
+def read_vcf(path):
+    """
+    Read a VCF file of alligned sequences
+    :param path: VCF file path
+    :return: Data frame of all data    
+    """
+    with open(path, 'r') as f:
+        lines = [l for l in f if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})
 
 def fetch_seq() -> str:
     """
