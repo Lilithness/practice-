594451759f027d8dfb6b1237cc7aabee4f20f484
Layal Shaheen <88163555+Lilithness@users.noreply.github.com>
2022-01-17 01:03:18 +0300

    Use argparse

--- generate_vep_variants.py
+++ generate_vep_variants.py
@@ -7,12 +7,21 @@ import os
 import sys
 import json
 from typing import Dict, List, Any, Sequence
+import argparse
 import requests
 import pandas as pd
 from corvus.cmd import get_cmd_output  # type: ignore
 
 
-## TODO: annotate the return value
+parser = argparse.ArgumentParser(description='Get descriptions about the variants found between two DNA sequences')
+parser.add_argument('query', type=str, help='The path of the query file')
+parser.add_argument('symbol', type=str, help='The symbol of the gene of interest')
+parser.add_argument('transcript_id', type=str, help='The transcript id of interest ')
+args = parser.parse_args()
+
+# TODO: annotate the return value
+
+
 def read_vcf(path: str):
     """
     Read a VCF file
@@ -48,10 +57,12 @@ def fetch_seq(symbol: str) -> None:
         file.write(req.text)
 
 
-def main(args) -> None:
+def main(query, symbol, transcript_id) -> None:
     """
     Check if a DNA sequence has a mutation in the hemoglobin-Beta gene
-    :param args: list that contains the name of the file
+    :param query: query file path
+            symbol: gene symbol
+            id: transcript id
     :return: None
     """
 
@@ -59,20 +70,19 @@ def main(args) -> None:
     pd.set_option('display.max_rows', None)
     pd.set_option('display.width', 1000)
 
-    ## TODO: use argparse
-    query = args[0]  # Expected to be the name of a FASTA file
+#    query = args[0]  # Expected to be the name of a FASTA file
     if not os.path.exists(query):
-        fetch_seq(args[1])  # Expected to be the gene symbol
+        fetch_seq(symbol)  # Expected to be the gene symbol
         query = "generated_query.fsa"
     sample_path = "test/Sample.fsa"  # should i make it an input?
-    alignment_result = f'alignment_result_{args[1]}.vcf'  # the name of the allignment file depending on the gene symbol
+    alignment_result = f'alignment_result_{symbol}.vcf'  # the name of the allignment file depending on the gene symbol
     get_cmd_output(f"bio align {query} {sample_path} --vcf > {alignment_result}")  # check if the colomn option was necessary
 
     print(f"Working with input file: '{query}'")
 
-    ## TODO: Fetch the region and the genomic coordinate offset ("start") from Ensembl
+    # TODO: Fetch the region and the genomic coordinate offset ("start") from Ensembl
     url = "https://rest.ensembl.org/lookup/symbol/homo_sapiens/"  # to get the needed info about the gene chosen to get a sequence from ensembl
-    req = requests.get(url + args[1], headers={"Content-Type": "application/json"})
+    req = requests.get(url + symbol, headers={"Content-Type": "application/json"})
     req.raise_for_status()
     decoded = req.json()
     strpos = int(decoded["start"])
@@ -114,7 +124,7 @@ def main(args) -> None:
                 item for item in example['transcript_consequences'] if isinstance(item, dict)
             ]       # change the value of thed dictionary to be a dictionary
             example['significant_transcript_consequences'] = [
-                item for item in example['transcript_consequences'] if item['transcript_id'] == args[2]
+                item for item in example['transcript_consequences'] if item['transcript_id'] == transcript_id  # ENST00000335295
             ][0]       # item is a dict # get only the dictionaries that contain a specific value for transcript id key
 
             consequences_list.append(example['significant_transcript_consequences'])
@@ -158,11 +168,11 @@ def main(args) -> None:
             },
             inplace=True
         )
-        
-        filename = f'data_file_{args[1]}.csv'
+
+        filename = f'data_file_{symbol}.csv'
         new.to_csv(filename, index=False)
         print(new)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(args.query, args.symbol, args.transcript_id)
