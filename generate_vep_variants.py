71be29165f058a841c11beb7368f13b95b0beb51
Layal Shaheen <88163555+Lilithness@users.noreply.github.com>
2022-01-18 17:01:36 +0300

    Add function to randomize query file

--- generate_vep_variants.py
+++ generate_vep_variants.py
@@ -3,19 +3,22 @@
 Get descriptions about the variants found between two DNA sequences.
 """
 
-import io
 import os
+import io
 import sys
 import json
 from typing import Dict, List, Any, Sequence
+import shutil
 import argparse
+import random
 from datetime import datetime
 import requests
 import pandas as pd
 from corvus.cmd import get_cmd_output  # type: ignore
 
-timestamp = datetime.now()
-## ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ##
+timestamp = datetime.now().time()
+
+
 def read_vcf(path: str) -> pd.DataFrame:
     """
     Read a VCF file
@@ -33,9 +36,32 @@ def read_vcf(path: str) -> pd.DataFrame:
     ).rename(columns={'#CHROM': 'CHROM'})
 
 
-## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ##
-# TODO: rename this func to reflect the fact that we are getting gene metadata, not sequences
-def symbol_database(symbol: str) -> None:
+def randomizer(filepath) -> None:
+    """
+    :param: Path of file
+    Change 10 nucleotides in a FASTA file to generate a random query
+    """
+    nuc = ['A', 'C', 'G', 'T']
+
+    def random_nuc():
+        """
+        Chose a random nucleotide
+        :return: Nucleotide
+        """
+        return random.choice(nuc)
+
+    with open(filepath, 'r', encoding='ascii') as edit:
+        header = next(edit)  # this case delets the header
+        data = list(edit.read())
+        for i in range(10):
+            ran = random.randint(0, len(data)-1)
+            data[ran] = random_nuc()
+    with open(filepath, 'w', encoding='ascii') as file:
+        file.write(header)
+        file.write(''.join(data))
+
+
+def symbol_database(symbol: str) -> Dict:
     """
     Find the species and database for a symbol in a linked external database
     :param: Gene symbol
@@ -45,7 +71,9 @@ def symbol_database(symbol: str) -> None:
     req = requests.get(url + symbol, headers={"Content-Type": "application/json"})
     req.raise_for_status()
     return req.json()
-def get_reference(symbol: str) -> None:
+
+
+def get_reference(symbol: str) -> str:
     """
     Get the reference sequence as a FASTA file from Ensembl
     :param: Gene symbol
@@ -58,9 +86,6 @@ def get_reference(symbol: str) -> None:
     return req.text
 
 
-
-
-## ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ##
 def main() -> None:
     """
     Check if a DNA sequence has a mutation in the hemoglobin-Beta gene
@@ -71,24 +96,33 @@ def main() -> None:
     """
 
     parser = argparse.ArgumentParser(description='Get descriptions about the variants found between two DNA sequences')
-    parser.add_argument('-q', '--query', type=str, help='Path to the query FASTA file',
-                        required=True)  # TODO: implement support for FASTQ
+    parser.add_argument('-q', '--query', type=str, help='Path to the query FASTA file')  # TODO: implement support for FASTQ
     parser.add_argument('-s', '--symbol', type=str, help='Symbol of the gene of interest', required=True)
     parser.add_argument('-t', '--enst', type=str, help='Ensemble transcript id of interest', required=True)
+    parser.add_argument('-d', '--demo', type=bool, help='Use a random sequence as the query')
     args = vars(parser.parse_args())
 
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
-    pd.set_option('display.width',100) #TODO: change this number to smth more reasonable
-
+    pd.set_option('display.width', 1000)
 
     reference_file = f'{args["symbol"]}_reference.fsa'
     with open(reference_file, "w", encoding='ascii') as file:
         textfile = get_reference(args["symbol"])
         file.write(textfile)
 
-    alignment_result = f'{args["symbol"]}{timestamp}_.vcf'  # the name of the alignment file depending on the gene symbol
-    output = get_cmd_output(f"bio align {args['query']} {reference_file} --vcf > {alignment_result}")  # check if the colomn option was necessary
+    if not args['query'] and args['demo'] is True:
+        original = reference_file
+        copy = f'test/{reference_file}'
+        shutil.copyfile(original, copy)
+        query = f'test/{args["symbol"]}_query_{timestamp}'
+        os.rename(copy, query)
+        randomizer(query)
+    else:
+        query = args['query']
+
+    alignment_result = f'{args["symbol"]}.vcf'  # the name of the alignment file depending on the gene symbol
+    output = get_cmd_output(f"bio align {query} {reference_file} --vcf > {alignment_result}")  # check if the colomn option was necessary
 
     if output["rc"]:
         print("[ stderr ]")
@@ -99,8 +133,7 @@ def main() -> None:
     for line in output["stdout"]:
         print(line)
 
-    print(f"Working with input file: '{args['query']}'")
-
+    print(f"Working with input file: '{query}'")
 
     decoded = symbol_database(args['symbol'])
     vcf_df = read_vcf(alignment_result)
@@ -108,11 +141,12 @@ def main() -> None:
 
     variants: List = []  # list of all possible variants of the sequences in the specific right format
     for row in vcf_df.itertuples():
+        print(row)
         region = row.CHROM
         chrom = region.split(":")[2]
         strand = region.split(":")[-1]
         end_pos = row.POS + len(row.REF) - 1
-        line = f"{chrom} {row.POS} {end_pos} {row.REF}/{row.ALT} {strand}"  # TODO: check if smth really requires the forward slash downstream -> Crucial for the format of the variant
+        line = f"{chrom} {row.POS} {end_pos} {row.REF}/{row.ALT} {strand}"
         variants.append(line)
 
     jdata = json.dumps({
@@ -140,7 +174,7 @@ def main() -> None:
                 item for item in example['transcript_consequences'] if isinstance(item, dict)
             ]       # change the value of the dictionary to be a dictionary
 
-            ## Extract consequences into a list of dictionaries to be appended to the main variant list datastructure
+            # Extract consequences into a list of dictionaries to be appended to the main variant list datastructure
             example['significant_transcript_consequences'] = [
                 item for item in example['dict_of_transcript_consequences'] if item['transcript_id'] == args['enst']  # ENST00000335295
             ][0]       # item is a dict # get only the dictionaries that contain a specific value for transcript id key
@@ -148,7 +182,6 @@ def main() -> None:
 
             if 'regulatory_feature_consequences' in example:
                 example['rf_cons'] = example['regulatory_feature_consequences'][0]  # to correct the format of the output.
-
 
             example['Ref'] = example['allele_string'].split("/")[0]
             assembled_dict = {**example, **consequences_list[i]}  # adding data to the dict
@@ -182,12 +215,11 @@ def main() -> None:
             inplace=True
         )
 
-        ## TODO: use query filename instead and add .annotated.csv postfix+extension
+        # TODO: use query filename instead and add .annotated.csv postfix+extension
         filename = f'{args["symbol"]}_{timestamp}.annotated.csv'
         output_df.to_csv(filename, index=False)
         print(output_df)
 
 
-## ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ##
 if __name__ == "__main__":
     main()
